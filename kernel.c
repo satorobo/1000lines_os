8be04b5fcac88f4802fa8da12a11b8e0eb34efae
Satoshi Suda <sato.robo.0729@gmail.com>
2024-01-10 13:42:14 +0900

    Implemented the kernel panic.

--- kernel.c
+++ kernel.c
@@ -29,12 +29,8 @@ void putchar(char ch) {
 void kernel_main(void) {
   memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-  printf("\n\nHello %s\n", "World!");
-  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-
-  for (;;) {
-    __asm__ __volatile__("wfi");
-  }
+  PANIC("booted!");
+  printf("unreachable here!\n");
 }
 
 __attribute__((section(".text.boot")))
