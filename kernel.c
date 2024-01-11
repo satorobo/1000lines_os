3dd1abd66638d9cc630b3c5954dce6156b4cef7c
Satoshi Suda <sato.robo.0729@gmail.com>
2024-01-11 18:13:34 +0900

    Revised trap handler to use each process's kernel stack.

--- kernel.c
+++ kernel.c
@@ -85,7 +85,7 @@ void switch_context(uint32_t *prev_sp, uint32_t *next_sp) {
     "sw s9,  10 * 4(sp)\n"
     "sw s10, 11 * 4(sp)\n"
     "sw s11, 12 * 4(sp)\n"
-    "sw sp, (a0)\n" // a0 stores *prev_sp
+    "sw sp, (a0)\n" // a0 stores *prev_sp -> update *prev_sp
     "lw sp, (a1)\n" // a1 stores *next_sp
     "lw ra,  0  * 4(sp)\n"
     "lw s0,  1  * 4(sp)\n"
@@ -116,6 +116,12 @@ void yield(void) {
 
   if (next == current_proc)
     return;
+
+  __asm__ __volatile__(
+    "csrw sscratch, %[sscratch]\n"
+    :
+    : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+  );
 
   struct process *prev = current_proc;
   current_proc = next;
@@ -168,7 +174,8 @@ __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
   __asm__ __volatile__(
-    "csrw sscratch, sp\n"
+    "csrrw sp, sscratch, sp\n"
+
     "addi sp, sp, -4 * 31\n"
     "sw ra,  4 * 0(sp)\n"
     "sw gp,  4 * 1(sp)\n"
@@ -203,6 +210,9 @@ void kernel_entry(void) {
 
     "csrr a0, sscratch\n"
     "sw a0, 4 * 30(sp)\n"
+
+    "addi a0, sp, 4 * 31\n"
+    "csrw sscratch, a0\n"
 
     "mv a0, sp\n"
     "call handle_trap\n"
