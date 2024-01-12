9b661ba285ea8d5a3c43be9d81bc87393980aa7f
Satoshi Suda <sato.robo.0729@gmail.com>
2024-01-12 19:29:14 +0900

    Added a first application.

--- run.sh
+++ run.sh
@@ -4,10 +4,18 @@ set -xue
 QEMU=qemu-system-riscv32
 
 CC=/usr/bin/clang
+OBJCOPY=/usr/bin/llvm-objcopy
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
+# Build shell
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
+# Build kernel
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+  kernel.c common.c shell.bin.o
 
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
   -kernel kernel.elf
